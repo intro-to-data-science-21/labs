f226bd812f93f446736dd8e003e9d69e1bc017dd
Lisa Oswald <lisao.kassel@gmail.com>
2021-09-22 07:15:38 +0200

    update path script

--- session-2-version-control/path-test.R
+++ session-2-version-control/path-test.R
@@ -1,12 +1,10 @@
 # Demonstration of relative and absolute paths
+library(readr)
 
 # create some data
 
-x <- c(1:20)
-y <- c(letters[1:20])
-z <- rnorm(20,1,100)
+my_df <- data.frame(id = letters[1:10], x = 1:10, y = 11:20)
 
-my_df <- data.frame(x,y,z)
 
 # save data as csv 
 
@@ -14,13 +12,13 @@ my_df <- data.frame(x,y,z)
 write.csv(my_df, "my_df.csv")
 
 # b) in data folder of working directory (EVEN BETTER)
-write.csv(my_df, "data/my_df.csv")
-write.csv(my_df, "./data/my_df.csv") #same
+write.csv(my_df, "data/my_df.csv", row.names=FALSE)
+write.csv(my_df, "./data/my_df.csv", row.names=FALSE) #same
 
 # c) by specifying the absolute path (WRONG)
-write.csv(my_df,"/Users/l.oswald/Documents/R/Session-2-test/data/my_df2.csv")
+write.csv(my_df,"/Users/l.oswald/Documents/R/Session-2-test/data/my_df2.csv", row.names=FALSE)
 # abbreviate root dirctory - a little bit shorter but still absolute - no one has your folder structure!
-write.csv(my_df,"~/Documents/R/Session-2-test/data/my_df2.csv") 
+write.csv(my_df,"~/Documents/R/Session-2-test/data/my_df2.csv", row.names=FALSE) 
 
 
 # read csv
@@ -28,6 +26,6 @@ write.csv(my_df,"~/Documents/R/Session-2-test/data/my_df2.csv")
 # absolute path
 my_read_df2 <- read_csv("/Users/l.oswald/Documents/R/Session-2-test/data/my_df2.csv")
 # relative path
-my_read_df <- read_csv("my_df.csv") # better, right?
+my_read_df <- read_csv("data/my_df.csv") # better, right?
 
 
