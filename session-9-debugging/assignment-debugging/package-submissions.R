9b3f6a1bee2b6661d45bc914fc18aa1ed1e58b99
Lisa Oswald <lisao.kassel@gmail.com>
2021-11-18 17:54:39 +0100

    session 9

---
+++ session-9-debugging/assignment-debugging/package-submissions.R
@@ -0,0 +1,93 @@
+devtools::install_github("adelaida-barrera/hertiestats2") 
+browseURL("https://github.com/adelaida-barrera/hertiestats2")
+library(hertiestats2)
+# installation failed
+
+
+devtools::install_github("anabelberjonsanchez/christmaspackage")
+browseURL("https://github.com/anabelberjonsanchez/christmaspackage")
+library(christmaspackage)
+# ReadMe missing
+?holiday
+holiday(christmas = FALSE)
+holiday(christmas = TRUE)
+
+
+devtools::install_github("GusMalija/skangagani")
+browseURL("https://github.com/GusMalija/skangagani")
+library(skangagani)
+?moto
+?npawa
+npawa(10)
+
+devtools::install_github("Bokyung130/awesomepackage")
+browseURL("https://github.com/Bokyung130/awesomepackage")
+library(awesomepackage)
+?animal_function
+animal_function(love = T)
+animal_function(love = F)
+
+
+devtools::install_github("brunoponne123/brleur")
+browseURL("https://github.com/brunoponne123/brleur")
+library(brleur)
+?brl_eur
+# installation failed
+
+
+devtools::install_github("ctrasi/firstpackage")
+browseURL("https://github.com/ctrasi/firstpackage")
+library(firstpackage)
+?my_sqrt
+my_sqrt
+# function not properly documented
+
+
+devtools::install_github("HertieGeorg/TestRunGP")
+browseURL("https://github.com/HertieGeorg/TestRunGP")
+library(TestRunGP)
+TestRunGP::double_function(12345)
+?double_function
+
+devtools::install_github("jiyoonhhan/veryokpackage")
+browseURL("https://github.com/jiyoonhhan/veryokpackage")
+library(veryokpackage)
+?best_cat
+
+devtools::install_github("Adugna96/PackageDS")
+browseURL("https://github.com/Adugna96/PackageDS")
+library(PackageDS)
+?Germ
+
+
+devtools::install_github("CocoPal/bestcolorgraphs")
+browseURL("https://github.com/CocoPal/bestcolorgraphs")
+library(bestcolorgraphs)
+?theme_purple
+ggplot(mtcars, aes(wt, mpg)) +
+	geom_point() +
+	labs(title = "Fuel economy declines as weight increases") + 
+	theme_purple()
+ggplot(mtcars, aes(wt, mpg)) +
+	geom_point() +
+	labs(title = "Fuel economy declines as weight increases") + 
+	theme_blue()
+
+
+devtools::install_github("sophieemeyer/minimalpkgrepo")
+browseURL("https://github.com/sophieemeyer/minimalpkgrepo")
+library(minimalpackage)
+?m_math
+
+
+
+devtools::install_github("vikiarn/functionfirst")
+browseURL("https://github.com/vikiarn/functionfirst")
+library(novelpackage)
+novelpackage::Christmas_function(love=FALSE)
+
+
+devtools::install_github("robertoquijanol/rqpackage")
+browseURL("https://github.com/robertoquijanol/rqpackage")
+library(rqpackage)
+
