e26539a46a90ee25fb2cae9a371c4cdde3431d5c
Tom Arend <tom_arend@hotmail.de>
2021-11-30 12:54:29 +0100

    Revised Exercises

--- session-10-shiny/app_plotly_solutions.R
+++ session-10-shiny/app_plotly_solutions.R
@@ -63,6 +63,14 @@ ui <- fluidPage(
                          0, 4, 0, step = 1),
              sliderInput("boxoffice", "Dollars at Box Office (millions)",
                          0, 800, c(0, 800), step = 1),
+             
+             #### Answer to Exercise 1:
+             selectInput("genre", "Genre (a movie can have multiple genres)",
+                         c("All", "Action", "Adventure", "Animation", "Biography", "Comedy",
+                           "Crime", "Documentary", "Drama", "Family", "Fantasy", "History",
+                           "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi",
+                           "Short", "Sport", "Thriller", "War", "Western")
+             ),
              textInput("director", "Director name contains (e.g., Miyazaki)"),
              textInput("cast", "Cast names contains (e.g. Tom Hanks)")
            ),
@@ -116,7 +124,13 @@ server <- function(input, output, session) {
       ) %>%
       arrange(Oscars)
     
-
+    
+    #### Answer to Exercise 1 
+    # Optional: filter by genre
+    if (input$genre != "All") {
+      genre <- paste0("%", input$genre, "%")
+      m <- m %>% filter(Genre %like% genre)
+    }
     # Optional: filter by director
     if (!is.null(input$director) && input$director != "") {
       director <- paste0("%", input$director, "%")
@@ -139,7 +153,7 @@ server <- function(input, output, session) {
     m
   })
   
-
+  
   
   # Function for generating tooltip text
   movie_tooltip <- function(x) {
@@ -173,6 +187,8 @@ server <- function(input, output, session) {
         y = yvar,
         fill = has_oscar,
         colour = has_oscar,
+        ### Answer to Exercise 2:
+        size = BoxOffice,
         text = paste0(
           "<b>", Title, "</b><br>",
           "Year: ", Year, "<br>",
@@ -180,15 +196,17 @@ server <- function(input, output, session) {
         )
       )) +
       geom_point(shape = 21) +
+      ### Answer to Exercise 2:
+      scale_size() +
       scale_fill_viridis(discrete=TRUE, option="C", name = "Won an Oscar", labels = c("Yes", "No"), alpha = 0.7) +
       scale_colour_viridis(discrete=TRUE, guide="none", option="C", alpha = 0.9) +
       theme_ipsum() +
       ylab(paste(yvar_name)) +
       xlab(paste(xvar_name)) +
       guides(fill=guide_legend(title="Won an Oscar"))
-      
+    
     ggplotly(p, tooltip = "text", height = 400)
-})
+  })
   
   output$n_movies <- renderText({ nrow(movies()) })
 }
