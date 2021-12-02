f7d99e4dfbb4a644521e602c31d2c0c97cf31ec2
Lisa Oswald <lisao.kassel@gmail.com>
2021-12-02 09:59:12 +0100

    minor

--- session-10-shiny/our_app/app_plotly_solutions.R
+++ session-10-shiny/our_app/app_plotly_solutions.R
@@ -6,6 +6,8 @@ library(hrbrthemes)
 library(RSQLite)
 library(dbplyr)
 
+
+
 # Setup -------------------------------------------------------------------
 
 
@@ -53,13 +55,21 @@ ui <- fluidPage(
            wellPanel(
              h4("Filter"),
              sliderInput("reviews", "Minimum number of reviews on Rotten Tomatoes",
-                         10, 200, 80, step = 10),
+                         10, 300, 80, step = 10),
              sliderInput("year", "Year released", 1940, 2014, value = c(1970, 2014),
                          sep = ""),
              sliderInput("oscars", "Minimum number of Oscar wins (all categories)",
                          0, 4, 0, step = 1),
              sliderInput("boxoffice", "Dollars at Box Office (millions)",
                          0, 800, c(0, 800), step = 1),
+             
+             #### Answer to Exercise 1:
+             selectInput("genre", "Genre (a movie can have multiple genres)",
+                         c("All", "Action", "Adventure", "Animation", "Biography", "Comedy",
+                           "Crime", "Documentary", "Drama", "Family", "Fantasy", "History",
+                           "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi",
+                           "Short", "Sport", "Thriller", "War", "Western")
+             ),
              textInput("director", "Director name contains (e.g., Miyazaki)"),
              textInput("cast", "Cast names contains (e.g. Tom Hanks)")
            ),
@@ -113,7 +123,13 @@ server <- function(input, output, session) {
       ) %>%
       arrange(Oscars)
     
-
+    
+    #### Answer to Exercise 1 
+    # Optional: filter by genre
+    if (input$genre != "All") {
+      genre <- paste0("%", input$genre, "%")
+      m <- m %>% filter(Genre %like% genre)
+    }
     # Optional: filter by director
     if (!is.null(input$director) && input$director != "") {
       director <- paste0("%", input$director, "%")
@@ -136,7 +152,7 @@ server <- function(input, output, session) {
     m
   })
   
-
+  
   
   # Function for generating tooltip text
   movie_tooltip <- function(x) {
@@ -170,6 +186,8 @@ server <- function(input, output, session) {
         y = yvar,
         fill = has_oscar,
         colour = has_oscar,
+        ### Answer to Exercise 2:
+        size = BoxOffice,
         text = paste0(
           "<b>", Title, "</b><br>",
           "Year: ", Year, "<br>",
@@ -177,15 +195,17 @@ server <- function(input, output, session) {
         )
       )) +
       geom_point(shape = 21) +
+      ### Answer to Exercise 2:
+      scale_size() +
       scale_fill_viridis(discrete=TRUE, option="C", name = "Won an Oscar", labels = c("Yes", "No"), alpha = 0.7) +
       scale_colour_viridis(discrete=TRUE, guide="none", option="C", alpha = 0.9) +
       theme_ipsum() +
       ylab(paste(yvar_name)) +
       xlab(paste(xvar_name)) +
       guides(fill=guide_legend(title="Won an Oscar"))
-      
+    
     ggplotly(p, tooltip = "text", height = 400)
-})
+  })
   
   output$n_movies <- renderText({ nrow(movies()) })
 }
